1bbf59ff0a3e68d82370a64b2c0501e6c24b989d
soulqan <soultan.agnar@gmail.com>
2023-09-15 13:55:54 +0700

    P3_percobaan1_pertanyaan1

--- src/P3/ContohVariabel29.java
+++ src/P3/ContohVariabel29.java
@@ -2,16 +2,16 @@ package P3;
 
 public class ContohVariabel29 {
     public static void main(String[] args) {
-        String salahSatuHobySayaAdalah="Bermain Petak Umpet";
-        boolean isPandai = true;
+        String hoby="Bermain Petak Umpet";
+        boolean pandai = true;
         char jenisKelamin = 'L';
-        byte _umurSayaSekarang = 20;
+        byte umur = 20;
         double $ipk=3.24, tinggi= 1.78 ;
         
-        System.out.println(salahSatuHobySayaAdalah);
-        System.out.println("Apakah Pandai?"+ isPandai);
+        System.out.println(hoby);
+        System.out.println("Apakah Pandai?"+ pandai);
         System.out.println("Jenis Kelamin:" + jenisKelamin);
-        System.out.println("Umurku saat ini:"+ _umurSayaSekarang);
+        System.out.println("Umurku saat ini:"+ umur);
         System.out.println(String.format("saya beripk %s, dengan tinggi badan %s", $ipk,tinggi));
     }
     
