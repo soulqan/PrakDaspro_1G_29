11c68a9aaa9f58f4b910616592bd1bae11977edf
soulqan <soultan.agnar@gmail.com>
2023-09-15 15:06:42 +0700

    p3_percobaan3_29

--- src/P3/ContohTipeData29.java
+++ src/P3/ContohTipeData29.java
@@ -5,7 +5,7 @@ public class ContohTipeData29 {
         char golonganDarah='A';
         byte jarak =(byte) 130;
         short jumlahPendudukDalamSatuDusun = 1025;
-        float suhu= 60.50F;
+        float suhu= 60.50f;
         double berat =0.5467812345;
         long saldo = 150000000;
         int angkaDesimal = 0x10;
