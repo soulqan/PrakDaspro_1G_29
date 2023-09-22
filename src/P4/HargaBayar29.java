6d494d2a1ac7e3a1a4aa3aa0a24d99c460368f38
soulqan <soultan.agnar@gmail.com>
2023-09-22 15:40:41 +0700

    P4_percobaan3_29

---
+++ src/P4/HargaBayar29.java
@@ -0,0 +1,22 @@
+package P4;
+
+import java.util.Scanner;
+
+public class HargaBayar29 {
+    public static void main(String[] args) {
+        Scanner inpuut29=new Scanner(System.in);
+
+    int harga,jumlah;
+    double dis=0.1, total, bayar, jmlDis;
+    System.out.println("Masukkan harga barang yang dibeli");
+    harga=inpuut29.nextInt();
+    System.out.println(" Masukkan Jumlah jumlah barang yang dibeli");
+    jumlah=inpuut29.nextInt();
+    total=harga*jumlah;
+    jmlDis=total*dis;
+    bayar=total-jmlDis;
+    System.out.println("Diskon yang anda dapatkan adalah "+jmlDis);
+    System.out.println("Jumlah yang harus dibayar adalah "+bayar);
+
+    }
+}
