63ca1cc03c3fba7b878514706767124eced650ef
soulqan <soultan.agnar@gmail.com>
2023-09-22 15:31:05 +0700

    P4_percobaan2_pertanyaan2_29

--- src/P4/Gaji29.java
+++ src/P4/Gaji29.java
@@ -4,14 +4,19 @@ public class Gaji29 {
     public static void main(String[] args) {
         Scanner input29= new Scanner(System.in);
 
-        int jmlMasuk,jmlTidakMasuk,totGaji,gaji=40000,potGaji=25000;
+        int jmlMasuk,jmlTidakMasuk,totGaji,gaji,potGaji;
 
-        System.out.println("Masukkan Jumlah hari anda masuk: ");
+        System.out.println("Masukkan jumlah hari anda masuk: ");
         jmlMasuk=input29.nextInt();
-        System.out.println("Masukkan Jumlah hari tidak masuk:" );
+        System.out.println("Masukkan jumlah hari tidak masuk:" );
         jmlTidakMasuk=input29.nextInt();
-        totGaji=(jmlMasuk*gaji*jmlTidakMasuk*potGaji);
-        System.out.println("Gaji yang anda terima"+totGaji);
+        System.out.println("Masukkan jumlah gaji: ");
+        gaji=input29.nextInt();
+        System.out.println("Masukkan jumlah potongan gaji: ");
+        potGaji=input29.nextInt();
+        totGaji=(jmlMasuk*gaji)-(jmlTidakMasuk*potGaji);
+        System.out.println("Gaji yang anda terima adalah  "+totGaji);
     }
 
 }
+
