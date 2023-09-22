aac73eeec9eceeb2d7a5a998b34562480ad96024
soulqan <soultan.agnar@gmail.com>
2023-09-22 14:04:40 +0700

    P4_percobaan1_29

---
+++ src/P4/Gaji29.java
@@ -0,0 +1,13 @@
+package P4;
+import java.util.Scanner;
+public class Gaji29 {
+    public static void main(String[] args) {
+        Scanner input29= new Scanner(System.in);
+
+        int jmlMasuk,jmlTidakMasuk,totGaji,gaji=40000,potGaji=25000;
+
+        System.out.println("Masukkan jumlah hari anda masuk: ");
+        
+        System.out.println("Masukkan jumlah hari tidak masuk:" );
+    }
+}
---
+++ src/P4/Lingkaran29.java
@@ -0,0 +1,21 @@
+package P4;
+
+import java.util.Scanner;
+
+public class Lingkaran29 {
+    public static void main(String[] args) {
+        Scanner input29 = new Scanner(System.in);
+    int r;
+    double keliling,luas;
+
+    System.out.println("Masukkan jari-jari lingkaran: ");
+    r= input29.nextInt();
+    keliling=2*3.14*r;
+    luas=3.14*r*r;
+    System.out.println(keliling);
+    System.out.println(luas);
+    }
+    
+    
+ 
+}
