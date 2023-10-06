78d3ac3c8c15b686c59fc93fc540e205a289a116
soulqan <soultan.agnar@gmail.com>
2023-10-06 13:50:10 +0700

    P5_Percobaan1-29

--- src/P5/PemilihanPercobaan1_29.java
+++ src/P5/PemilihanPercobaan1_29.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class PemilihanPercobaan1_29 {
     public static void main(String[] args) {
         Scanner input29=new Scanner(System.in);
-        System.out.print("masukan angka:  ");
+        System.out.print("Masukan angka:  ");
     int angka = input29.nextInt();
-
-    String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
-    System.out.println("Angka ini adalah "+ hasil);
-
+    if(angka%2==0){
+        System.out.println("Angka "+ angka+" bilangan genap");
+    
+    }else{
+        System.out.println("Angka "+angka+" bilangan ganjil");
+    
+    }
+    }
+    
 }
-}
+
 
 
     
