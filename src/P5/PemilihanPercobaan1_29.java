7ac3a30ea2746c7701838c58dcd1fa2ef69aa71e
soulqan <soultan.agnar@gmail.com>
2023-10-06 13:47:13 +0700

    P5_Percobaan1

---
+++ src/P5/PemilihanPercobaan1_29.java
@@ -0,0 +1,19 @@
+package P5;
+
+import java.util.Scanner;
+
+public class PemilihanPercobaan1_29 {
+    public static void main(String[] args) {
+        Scanner input29=new Scanner(System.in);
+        System.out.print("masukan angka:  ");
+    int angka = input29.nextInt();
+
+    String hasil = (angka % 2 == 0) ? "Genap" : "Ganjil";
+    System.out.println("Angka ini adalah "+ hasil);
+
+}
+}
+
+
+    
+
