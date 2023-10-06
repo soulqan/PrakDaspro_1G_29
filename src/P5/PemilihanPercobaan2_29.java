b701a6cda12402a2333becf9e8e6d0ce10e75b7d
soulqan <soultan.agnar@gmail.com>
2023-10-06 14:30:24 +0700

    P5_percobaan2_Pertanyaan1_29

--- src/P5/PemilihanPercobaan2_29.java
+++ src/P5/PemilihanPercobaan2_29.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class PemilihanPercobaan2_29 {
     public static void main(String[] args) {
         Scanner input29= new Scanner(System.in);
-
+        String nilaiHuruf;
         System.out.println("Nilai UAS     :");
         float uas = input29.nextFloat();
         System.out.println("Nilai UTS     :");
@@ -20,6 +20,28 @@ public class PemilihanPercobaan2_29 {
         String message = total < 65 ? "Remidi" : "tidak Remidi";
 
         System.out.println("Nilai Akhir = "+total+ " sehingga "+message);
-
+        if (total > 80) {
+            nilaiHuruf = "A"; 
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }else if (total > 73){
+            nilaiHuruf = "B+";
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }else if(total > 65){
+            nilaiHuruf = "B";
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }else if(total > 60){
+            nilaiHuruf = "C+";
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }else if (total > 50) {
+            nilaiHuruf = "C";
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }else if (total > 39) {
+            nilaiHuruf = "D";
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }else if (total <= 39) {
+            nilaiHuruf = "E";
+            System.out.println("Nilai Huruf Anda Adalah : "+ nilaiHuruf );
+        }
     }
+
 }
