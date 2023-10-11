650961b89bdb33243d1894c7bbd8e466b4c46279
soulqan <soultan.agnar@gmail.com>
2023-10-11 18:34:20 +0700

    P6_Percobaan1_Pertanyaan3_29

--- src/P6/Pemilihan2Percobaan1_29.java
+++ src/P6/Pemilihan2Percobaan1_29.java
@@ -1,19 +1,17 @@
 package P6;
-
 import java.util.Scanner;
 
 public class Pemilihan2Percobaan1_29 {
     public static void main(String[] args) {
-        Scanner input29=new Scanner(System.in);
-        int tahun;
-        
-        System.out.println("masukkan tahun: ");
-        tahun=input29.nextInt();
-        if (tahun%4==0) {
-            if (tahun%100!=0) 
-                System.out.println("tahun Kabisat");
-        }else 
-            System.out.println("Bukan Tahun Kabisat");
+        Scanner input29 = new Scanner (System.in);
+            System.out.print("Massukkan Angka= ");
+        int tahun = input29.nextInt();
+        if (( tahun %4)==0&&(tahun % 100)!=0){
+            //if ((tahun % 100)!=0);
+            
+            System.out.println("Tahun Kabisat");
+        } else 
+            System.out.println( " Bukan tahun kabisat ");
+    
 
-    }
-}
+    }}
