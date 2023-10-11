65713a56a23692d036ec532d6ffbbf4ba368c8f7
soulqan <soultan.agnar@gmail.com>
2023-10-11 18:46:14 +0700

    P6_Percobaan2_29

---
+++ src/P6/Pemilihan2Percobaan2_29.java
@@ -0,0 +1,28 @@
+package P6;
+
+import java.util.Scanner;
+
+public class Pemilihan2Percobaan2_29 {
+    public static void main(String[] args) {
+        Scanner input29=new Scanner(System.in);
+        float sudut1,sudut2,sudut3,totalSudut;
+
+        System.out.println("Masukkan sudut ke-1 : ");
+        sudut1 =input29.nextFloat();
+        System.out.println("masukkan sudut ke-2 : ");
+        sudut2=input29.nextFloat();
+        System.out.println("masukkan sudut ke-3 : ");
+        sudut3=input29.nextFloat();
+        totalSudut=sudut1+sudut2+sudut3;
+
+        if (totalSudut==180) {
+            if (sudut1==90||sudut2==90||sudut3==90) {
+                System.out.println("segitiga siku siku");
+            }else
+            System.out.println("Bukan Segitiga Siku-Siku");
+        }else
+            System.out.println("Bukan Segitiga");
+        }
+
+    }
+
