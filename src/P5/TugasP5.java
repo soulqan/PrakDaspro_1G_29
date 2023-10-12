51eef7fc310b005274f0293b32a183cfc162e94b
soulqan <soultan.agnar@gmail.com>
2023-10-12 22:28:33 +0700

    tugas p5

--- src/P5/TugasP5.java
+++ src/P5/TugasP5.java
@@ -3,17 +3,25 @@ package P5;
 import java.util.Scanner;
 
 public class TugasP5 {
-       
     public static void main(String[] args) {
-    Scanner input29=new Scanner(System.in);
-    String opsiPengiriman;
-    
-    System.out.println("masukan ekspedisi:");
-    opsiPengiriman=input29.nextLine();
-    if (opsiPengiriman.equalsIgnoreCase("JNT")) {
-        System.out.println("ekspedisi yang anda pili adalah :"+opsiPengiriman);
-    }else if(opsiPengiriman.equalsIgnoreCase("JNE")){
-        System.out.println("ekspedisi yang anda pilih adalah : "+opsiPengiriman);
-        }
-    }
+        Scanner input29 = new Scanner(System.in);
+        int opsiPengiriman, metodePembayaran;
+
+        System.out.println("====== Pilihlah Opsi Pengiriman ======");
+        System.out.println("1. JNT \n2. JNE \n3. GoJek \n4. Grab ");
+        System.out.print("Pilihan: ");
+        opsiPengiriman = input29.nextInt();
+
+        if (opsiPengiriman == 1) {
+            System.out.println("Anda memilih opsi pengiriman JNT");
+        } else if (opsiPengiriman == 2) {
+            System.out.println("Anda memilih opsi pengiriman JNE");
+        } else if (opsiPengiriman == 3) {
+            System.out.println("Anda memilih opsi pengiriman GoJek");
+        } else if (opsiPengiriman == 4) {
+            System.out.println("Anda memilih opsi pengiriman Grab");
+        } else {
+            System.out.println("Tidak ada opsi pengiriman tersebut");
+ }
+}
 }
