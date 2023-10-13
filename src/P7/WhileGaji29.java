74a33f22be9195c288791c1636143d1323f90991
soulqan <soultan.agnar@gmail.com>
2023-10-13 13:46:00 +0700

    P7_percobaan2_29

---
+++ src/P7/WhileGaji29.java
@@ -0,0 +1,37 @@
+package P7;
+
+import java.util.Scanner;
+
+public class WhileGaji29 {
+    public static void main(String[] args) {
+        Scanner scan29= new Scanner(System.in);
+        int jumlahKaryawan,jumlahJamLembur;
+        double gajiLembur=0,totalGajiLembur=0;
+        String jabatan;
+
+        System.out.print("Masukkan jumlah karyawan: ");
+        jumlahKaryawan = scan29.nextInt();
+
+        int i = 0;
+
+        while (i < jumlahKaryawan) {
+            System.out.println("Pilihan jabatan - Direktur,Manajer, Karyawan");
+            System.out.print("Masukkan jabatan karyawan ke- "+(i+1) + ": ");
+            jabatan = scan29.next();
+            System.out.print("Masukkan jumlah jam lembur: ");
+            jumlahJamLembur = scan29.nextInt();
+            i++;
+
+            if (jabatan.equalsIgnoreCase("direktut")) {
+                continue;
+            }else if (jabatan.equalsIgnoreCase("manajer")) {
+                gajiLembur = (jumlahJamLembur * 100000);
+            }else if (jabatan.equalsIgnoreCase("karyawan")) {
+                gajiLembur = jumlahJamLembur * 75000;
+            }
+           
+        }
+        totalGajiLembur += gajiLembur;
+            System.out.println("Total gaji lembur: " + totalGajiLembur);
+}
+}
