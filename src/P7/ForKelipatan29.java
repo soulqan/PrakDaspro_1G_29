1acff9adf2a8e506c1b0a835f10859dcb68048b5
soulqan <soultan.agnar@gmail.com>
2023-10-13 14:18:30 +0700

    P7_percobaan1_Pertanyaan3_29

--- src/P7/ForKelipatan29.java
+++ src/P7/ForKelipatan29.java
@@ -6,7 +6,8 @@ public class ForKelipatan29 {
    
     public static void main(String[] args) {
         Scanner input29=new Scanner(System.in);
-        int kelipatan,jumlah=0,counter=0,total=0;
+        int kelipatan,counter=0,total=0;
+        
         
     System.out.print("Masukkan bilanagan kelipatan (1-9): ");
     kelipatan =input29.nextInt();
@@ -15,8 +16,10 @@ public class ForKelipatan29 {
         if (i % kelipatan == 0) {
             total+=i;
             counter++;
-            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah%d\n",kelipatan,counter);
+            double rata=total/counter;
+            System.out.printf("Banyaknya bilangan %d dari 1 sampai 50 adalah%d\n ",kelipatan,counter);
         System.out.printf("Total bilangan kelipatan %d dari 1 sampai 50 adalah%d\n", kelipatan,total);
+        System.out.println("rata- rata adalah : "+rata);
         }
     }
         
