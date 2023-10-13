77a6c6cce8a70c31db7e226f564e746ece7d6ec4
soulqan <soultan.agnar@gmail.com>
2023-10-13 13:51:59 +0700

    P7_Percobaan3_29

---
+++ src/P7/DoWhileCuti29.java
@@ -0,0 +1,31 @@
+package P7;
+
+import java.util.Scanner;
+
+public class DoWhileCuti29 {
+    public static void main(String[] args) {
+        Scanner scan29= new Scanner(System.in);
+    int jatahCuti,jumlahHari;
+    String konfirmasi;
+    
+        System.out.println("Jatah cuti: ");
+        jatahCuti = scan29.nextInt();
+
+        do{
+            System.out.println("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = scan29.next();
+            if ("Y".equalsIgnoreCase(konfirmasi)){
+                System.out.println("jumlah hari: ");
+                jumlahHari=scan29.nextInt();
+
+                if (jumlahHari<=jatahCuti) {
+                    jatahCuti-=jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                }else{
+                    System.out.println("Sisa jatah cuti Anda tidak mencukupi");
+                    break;
+                }
+        }
+    }while(jatahCuti>0);
+}
+}
