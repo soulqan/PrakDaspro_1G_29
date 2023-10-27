f3818bb1d8bac2b99ffa32f8c4eb8cb3a19bc6b5
soulqan <soultan.agnar@gmail.com>
2023-10-27 12:31:40 +0700

    P9_Percobaan2_29

---
+++ src/P9/ArrayNilai29.java
@@ -0,0 +1,18 @@
+package P9;
+
+import java.util.Scanner;
+
+public class ArrayNilai29 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] nilaiAkhir = new int[10];
+
+        for (int i=0; i<10; i++){
+            System.out.print("Masukkan Nilai Akhir ke-" + i+ " : ");
+            nilaiAkhir[i] = sc.nextInt();
+        }
+        for (int i = 0; i<10; i++){
+            System.out.println("Nilai Akhir ke-"+i+" adalah "+nilaiAkhir[i]);
+        }
+    }
+}
