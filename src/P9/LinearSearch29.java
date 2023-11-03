942b94157061853661f9866155d088da5c19cfcb
soulqan <soultan.agnar@gmail.com>
2023-11-03 16:09:20 +0700

    P9_Percobaan4_Pertanyaan2_29

--- src/P9/LinearSearch29.java
+++ src/P9/LinearSearch29.java
@@ -1,12 +1,20 @@
 package P9;
 
+import java.util.Scanner;
+
 public class LinearSearch29 {
     public static void main(String[] args) {
-        
-        int[] arrayInt = {34,18,26,48,72,20,56,63};
-        int key = 20;
-        int hasil = 0;
-
+        Scanner sc =new Scanner(System.in);
+        int hasil=0;        
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlah=sc.nextInt();
+        int[] arrayInt = new int[jumlah];
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.print("Masukkan elemen array ke-"+i+" : ");
+            arrayInt[i] = sc.nextInt();
+        }
+        System.out.println("Masukkan key yang ingin dicari: ");
+        int key=sc.nextInt();
         for(int i = 0;i<arrayInt.length;i++){
             if (arrayInt[i] == key){
                 hasil = i;
