a32752e74c6ae56d2b31b9de0c7e33988a2e5cbc
soulqan <soultan.agnar@gmail.com>
2023-11-03 13:17:37 +0700

    P9_Percobaan1_Pertanyaan1_29

--- src/P9/ArrayBilangan29.java
+++ src/P9/ArrayBilangan29.java
@@ -2,7 +2,11 @@ package P9;
 
 public class ArrayBilangan29 {
     public static void main(String[] args) {
-    int[] bil = {5,13,-7,17};
+    int[] bil = new int[4];
+    bil[0] =  5.0;
+    bil[1] = 12867;
+    bil[2] = 7.5;
+    bil[3] = 2000000;
 
     System.out.println(bil[0]);
     System.out.println(bil[1]);
