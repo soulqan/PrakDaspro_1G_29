cb6a7e17e91cc36a86df0e8e294c14b8b17c9d5d
soulqan <soultan.agnar@gmail.com>
2023-11-10 14:22:48 +0700

    P10_Percobaan1_pertanyaan12_29

--- src/P10/Bioskop29.java
+++ src/P10/Bioskop29.java
@@ -25,14 +25,16 @@ public class Bioskop29 {
         }
         System.out.println("Penonton pada baris ke-3: ");
         
-        for (String i: penonton[2]) {
-            System.out.print(i);
+       for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" +(i+0)+": "+ String.join(",", penonton[i]));
+            
+        }
        
             
         }
             
         }
-        }
+        
     
     
     
