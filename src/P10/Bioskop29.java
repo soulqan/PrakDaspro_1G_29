cbd87d02ffc950c7864f8d73b28bca625e8db60a
soulqan <soultan.agnar@gmail.com>
2023-11-10 13:59:30 +0700

    P10_Percobaan1_Pertanyaan6_29

--- src/P10/Bioskop29.java
+++ src/P10/Bioskop29.java
@@ -19,10 +19,11 @@ public class Bioskop29 {
 
         System.out.println(penonton.length);
         
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("panjang baris ke-" + (i+1) + ": " + penonton[i].length);
+        for (String[] barisPenonton : penonton) {
+            System.out.println("panjang baris: "+barisPenonton.length);
             
+        }  
         }
     }
-    }
+    
     
