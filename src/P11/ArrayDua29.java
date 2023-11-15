fe417bb75927c20c504019f63cf1deeaa7ee69c5
soulqan <soultan.agnar@gmail.com>
2023-11-15 12:43:46 +0700

    P11_Percobaan4_Pertanyaan1_29

--- src/P11/ArrayDua29.java
+++ src/P11/ArrayDua29.java
@@ -12,55 +12,60 @@ public class ArrayDua29 {
         double[] rata = { 0, 0, 0, 0 };
 
         for (int i = 0; i < data.length; i++) {
-            System.out.printf("jam %s\n",jam[i]);
+            System.out.printf("jam %s\n", jam[i]);
             for (int j = 0; j < data[0].length; j++) {
                 switch (j) {
                     case 0:
                         System.out.print("Masukkan suhu: ");
                         data[i][j] = sc29.nextInt();
-                        if (min[j]>data[i][j]) {
-                            min[j]=data[i][j];
+                        if (min[j] > data[i][j]) {
+                            min[j] = data[i][j];
                         }
-                        total[j]+=data[i][j];
+                        total[j] += data[i][j];
                         break;
                     case 1:
                         System.out.print("Masukkan kelembaban udara: ");
                         data[i][j] = sc29.nextInt();
-                        if (min[j]>data[i][j]) {
-                            min[j]=data[i][j];
+                        if (min[j] > data[i][j]) {
+                            min[j] = data[i][j];
                         }
-                        total[j]+=data[i][j];
+                        total[j] += data[i][j];
                         break;
                     case 2:
                         System.out.print("Masukkan kecepatan angin: ");
                         data[i][j] = sc29.nextInt();
-                        if (min[j]>data[i][j]) {
-                            min[j]=data[i][j];
+                        if (min[j] > data[i][j]) {
+                            min[j] = data[i][j];
                         }
-                        total[j]+=data[i][j];
+                        total[j] += data[i][j];
                         break;
                     case 3:
                         System.out.print("Masukkan index UV: ");
                         data[i][j] = sc29.nextInt();
-                        if (min[j]>data[i][j]) {
-                            min[j]=data[i][j];
+                        if (min[j] > data[i][j]) {
+                            min[j] = data[i][j];
                         }
-                        total[j]+=data[i][j];
+                        total[j] += data[i][j];
                         break;
 
                 }
-                rata[j]=total[j]/data[0].length;
+                rata[j] = total[j] / data[0].length;
             }
         }
         System.out.println("---------------");
         for (int i = 0; i < data.length; i++) {
             for (int j = 0; j < data[0].length; j++) {
-                System.out.print(data[i][j]+" ");
+                System.out.print(data[i][j] + " ");
             }
             System.out.println("");
 
         }
         System.out.println("---------------");
+        System.out.println("Data Kecepatan Angin:");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println("Data ke-" + (i + 1) + ": " + data[i][2]);
+        }
+
         System.out.printf("Rata-rata suhu: %.2f\n", rata[0]);
         System.out.printf("Persetase kelembaban terendah: %s\n", min[1]);
     }
