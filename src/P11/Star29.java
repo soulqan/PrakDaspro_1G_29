97725f683e0d855f947364262397303acfbe1739
soulqan <soultan.agnar@gmail.com>
2023-11-15 10:59:02 +0700

    P11_Percobaan1_29

---
+++ src/P11/Star29.java
@@ -0,0 +1,17 @@
+package P11;
+
+import java.util.Scanner;
+
+public class Star29 {
+    public static void main(String[] args) {
+        Scanner sc29 = new Scanner (System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int N = sc29.nextInt();
+
+        for (int i = 0; i <= N; i++) {
+            System.out.print("*");
+            
+        }
+    }
+}
