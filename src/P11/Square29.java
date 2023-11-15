623731f51049dacafcfa8c354c9a648bd20d2c61
soulqan <soultan.agnar@gmail.com>
2023-11-15 11:00:13 +0700

    P11_Percobaan2_29

---
+++ src/P11/Square29.java
@@ -0,0 +1,20 @@
+package P11;
+
+import java.util.Scanner;
+
+public class Square29 {
+    public static void main(String[] args) {
+        Scanner sc29=new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N = ");
+        int n =sc29.nextInt();
+
+        for(int iOuter=1; iOuter<=n; iOuter++){
+        for (int i = 0; i <=n ; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        }
+    }
+    }
+
