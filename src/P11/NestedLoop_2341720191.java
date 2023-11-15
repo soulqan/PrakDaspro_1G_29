3ac1a4774db874dea9c150c243eb4708b49ec5ff
soulqan <soultan.agnar@gmail.com>
2023-11-15 12:05:02 +0700

    P11_Percobaan3_Pertanyaan2_29

--- src/P11/NestedLoop_2341720191.java
+++ src/P11/NestedLoop_2341720191.java
@@ -1,24 +1,52 @@
+// package P11;
+// import java.util.Scanner;
+// public class NestedLoop_2341720191 {
+//     public static void main(String[] args) {
+//         Scanner sc29=new Scanner(System.in);
+//         double[][] temps=new double[5][7];
+
+//         for (int i = 0; i < temps.length; i++) {
+//             System.out.println("Kota ke-"+i);
+//             for (int j = 0; j < temps[0].length; j++) {
+//                 System.out.println("Hari ke-"+(j+1) +": ");
+//                 temps[i][j]=sc29.nextDouble();
+//             }
+//             System.out.println();
+//         }
+//         for (int i = 0; i < temps.length; i++) {
+//             System.out.print("kota ke-" +(i+1)+ ": ");
+//             for (int j = 0; j < temps[0].length; j++) {
+//              System.out.println(temps[i][j]+ " ");   
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
 package P11;
 import java.util.Scanner;
+
 public class NestedLoop_2341720191 {
     public static void main(String[] args) {
-        Scanner sc29=new Scanner(System.in);
-        double[][] temps=new double[5][7];
+        Scanner sc29 = new Scanner(System.in);
+        double[][] temps = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
-            System.out.println("Kota ke-"+i);
+            System.out.println("Kota ke-" + i);
             for (int j = 0; j < temps[0].length; j++) {
-                System.out.println("Hari ke-"+(j+1) +": ");
-                temps[i][j]=sc29.nextDouble();
+                System.out.println("Hari ke-" + (j + 1) + ": ");
+                temps[i][j] = sc29.nextDouble();
             }
             System.out.println();
         }
+
+        // Menampilkan array menggunakan foreach
         for (int i = 0; i < temps.length; i++) {
-            System.out.print("kota ke-" +(i+1)+ ": ");
-            for (int j = 0; j < temps[0].length; j++) {
-             System.out.println(temps[i][j]+ " ");   
+            System.out.print("Kota ke-" + (i + 1) + ": ");
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
             }
             System.out.println();
         }
     }
 }
+
