182aeb673d94fce19df9ee056f83b321f16221e8
soulqan <soultan.agnar@gmail.com>
2023-12-01 09:01:54 +0700

    P13_Percobaan2_29

--- src/P13/TerimaKasih_29.java
+++ src/P13/TerimaKasih_29.java
@@ -6,12 +6,14 @@ public class TerimaKasih_29 {
                 + "You inspired in me a love for learning and made me feel like u could ask you anything.");
     }
 
+public static void UcapanTamabahan(String ucapan){
+    System.out.println(ucapan);
+}
+
     public static void main(String[] args) {
         // Menggunkan fungsi
         UcapanTerimakasih();
-        // Tidak menggunakan fungsi
-        System.out.println("Thank You for being the best teacher in the world.\n"
-                + "You inspired in me a love for learning and made me feel like u could ask you anything.");
-
+        String ucapan="Terima Kasih Pak.. Bu.. Semoga Sehat Selalu";
+        UcapanTamabahan(ucapan);
     }
 }
