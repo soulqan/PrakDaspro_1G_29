f6360b2c46de2dc21b6116df2a1eb2f3b06fbd0b
soulqan <soultan.agnar@gmail.com>
2023-12-08 14:09:47 +0700

    P14_Percobaan2_29

---
+++ src/P14/Percobaan2.java
@@ -0,0 +1,22 @@
+package P14;
+
+import java.util.Scanner;
+
+public class Percobaan2 {
+    static int hitungPangkat(int x,int y){
+        if(y==0){
+            return(1);
+        }else{
+            return(x*hitungPangkat(x, y-1));
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int bilangan,pangkat;
+        System.out.println("Bilangan yang dihitung: ");
+        bilangan=sc.nextInt();
+        System.out.println("Pangkat: ");
+        pangkat=sc.nextInt();
+        System.out.println(hitungPangkat(bilangan, pangkat));
+    }
+}
