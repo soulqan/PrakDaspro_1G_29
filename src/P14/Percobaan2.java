3adde5c28f0b6b22a0ec4bd51689155d6d8c650f
soulqan <soultan.agnar@gmail.com>
2023-12-08 14:48:16 +0700

    P14_Percobaan2_Pertanyaan2_29

--- src/P14/Percobaan2.java
+++ src/P14/Percobaan2.java
@@ -3,20 +3,24 @@ package P14;
 import java.util.Scanner;
 
 public class Percobaan2 {
-    static int hitungPangkat(int x,int y){
-        if(y==0){
-            return(1);
-        }else{
-            return(x*hitungPangkat(x, y-1));
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            System.out.print("1");
+            return (1);
+        } else {
+            System.out.print(x + "x");
+            return (x * hitungPangkat(x, y - 1));
         }
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int bilangan,pangkat;
+        Scanner sc = new Scanner(System.in);
+        int bilangan, pangkat;
         System.out.println("Bilangan yang dihitung: ");
-        bilangan=sc.nextInt();
+        bilangan = sc.nextInt();
         System.out.println("Pangkat: ");
-        pangkat=sc.nextInt();
-        System.out.println(hitungPangkat(bilangan, pangkat));
+        pangkat = sc.nextInt();
+        int hasil = hitungPangkat(bilangan, pangkat);
+        System.out.print(" = " + hasil);
     }
 }
